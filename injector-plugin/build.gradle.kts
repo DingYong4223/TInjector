de09f4bebf07a8d29d3c6e6ff6d7ad898064299b
delanding <delanding@tencent.com>
2025-12-19 18:11:08 +0800

    升级kts

--- injector-plugin/build.gradle.kts
+++ injector-plugin/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-gradle-plugin")
     id("kotlin")
+    id("maven-publish")
 }
 
 dependencies {
@@ -14,5 +15,29 @@ dependencies {
     testImplementation("com.google.truth:truth:0.42")
     testImplementation("androidx.annotation:annotation:1.0.0")
     testImplementation("com.google.testing.compile:compile-testing:0.15")
+}
+
+group = "com.tenpay.injector"
+version = rootProject.extra["publish_version"] as String
+
+// Maven 发布配置
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = "com.tenpay.injector"
+            artifactId = "injector-plugin"
+        }
+    }
+
+    repositories {
+        // maven {
+        //     url = uri("https://your-nexus-repo.com/repository/maven-releases/")
+        //     credentials {
+        //         username = "your-username"
+        //         password = "your-password"
+        //     }
+        // }
+    }
 }
 
